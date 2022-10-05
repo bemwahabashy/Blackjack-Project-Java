d3dd5836ed7ceab61a63af0ba47c6e8400e913dd
Bemwa Habashy <bemwahabashy@csu.fullerton.edu>
2022-10-05 12:03:30 -0700

    first commit

---
+++ Link.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+class Link {
+protected Link next;
+
+public Link getNext() { return next; }
+public void setNext(Link newnext) { next = newnext; }
+
+} // end class Link
